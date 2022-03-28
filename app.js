59e30d09d0e37a316eb5611a9f7a3fea34394eba
alejoaimar <alejoaimar99@gmail.com>
2022-03-27 22:50:19 -0300

    Creacion de paginas Home, Login y Register

--- app.js
+++ app.js
@@ -10,5 +10,13 @@ app.listen(3000, () => {
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/views/home.html"));
+  res.sendFile(path.join(__dirname, "/views/index.html"));
+});
+
+app.get("/login", function (req, res) {
+  res.sendFile(path.join(__dirname, "/views/login.html"));
+});
+
+app.get("/register", function (req, res) {
+  res.sendFile(path.join(__dirname, "/views/register.html"));
 });
