af7d404ea584f76659459d3ae228aa14c05e153f
alejoaimar <alejoaimar99@gmail.com>
2022-06-17 19:24:47 -0300

    Formulario de registro

--- src/controllers/users-controller.js
+++ src/controllers/users-controller.js
@@ -1,3 +1,12 @@
+const { validationResult } = require("express-validator");
+
+fs = require("fs");
+path = require("path");
+const bcrypt = require("bcryptjs");
+
+const usersFilePath = path.join(__dirname, "../data/users.json");
+const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
+
 module.exports = {
   login: (req, res) => {
     res.render("./users/login");
@@ -5,4 +14,38 @@ module.exports = {
   register: (req, res) => {
     res.render("./users/register");
   },
-};
+  processRegister: (req, res) => {
+    const resultValidation = validationResult(req);
+    if (resultValidation.errors.length > 0) {
+      return res.render("./users/register", {
+        errors: resultValidation.mapped(),
+        oldData: req.body
+      });
+    }
+
+    let encryptedPass = bcrypt.hashSync("password", 10);
+
+    const lastIndex = users.length - 1;
+    const lastUser = users[lastIndex];
+    const biggestId = lastUser ? lastUser.id : 0;
+    const newId = biggestId + 1;
+
+     const user = {
+       id: newId,
+       name: req.body.name,
+       email: req.body.email,
+       phone: req.body.phone,
+       password: encryptedPass,
+       avatar: req.body.avatar
+         ? "/images/users/avatar" + req.avatar.filename
+         : "/images/users/avatar/default-image.png",
+     };
+
+    users.push(user)
+
+    const jsonTxt = JSON.stringify(users, null, 2);
+    fs.writeFileSync(usersFilePath, jsonTxt, "utf-8");
+
+    res.redirect('/')
+  }
+}
--- src/routes/users-routes.js
+++ src/routes/users-routes.js
@@ -1,10 +1,44 @@
 const { Router } = require("express");
+const path = require("path");
+const multer = require("multer");
+
+const {body} = require('express-validator')
 
 const usersRouter = Router();
 
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, "../../public/images/users/avatar"),
+  filename: (req, file, cb) => {
+    const fileName =
+      `${Date.now()}_img${path.extname(file.originalname)}`;
+    cb(null, fileName);
+  },
+});
+
+const upload = multer({
+  storage,
+});
+
+
 const usersController = require("../controllers/users-controller");
+
+const validations = [
+    body('phone').notEmpty().withMessage('Tienes que indicar un numero de telefono'),
+    body('name').notEmpty().withMessage('Tienes que indicar un nombre'),
+    body('email').notEmpty().withMessage('Tienes que indicar un email'),
+    body('password').notEmpty().withMessage('Tienes que indicar una contrasena'),
+    body('avatar').custom((value, { req }) => {
+        let file = req.file;
+        if (!file) {
+            throw new Error('Tienes que subir una imagen')
+        }
+
+        return true
+    })
+]
 
 usersRouter.get("/login", usersController.login);
 usersRouter.get("/register", usersController.register);
+usersRouter.post("/user/register",upload.single('avatar'), validations, usersController.processRegister)
 
 module.exports = usersRouter;
